1d2d0a8d7a8959494a1ae4be495d96b7f02e1fde
tomiconicgit <tomiconicx@icloud.com>
2025-11-08 18:01:16 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js
 
-const CACHE_VERSION = 'v1.0.3'; // <-- BUMPED VERSION
+const CACHE_VERSION = 'v1.0.4'; // <-- BUMPED VERSION
 const CACHE_NAME = `terra-pwa-cache-${CACHE_VERSION}`;
 
 const FILES_TO_CACHE = [
@@ -12,13 +12,12 @@ const FILES_TO_CACHE = [
     'src/main.js',
     'src/core/viewport.js',
     'src/core/camera.js',
-    
-    // --- ADDED ALL NEW MODULES ---
     'src/core/filemanagement.js',
     'src/core/procedural/terrain.js',
     'src/core/ui/menu.js',
-    'src/core/ui/workspace.js',
-    'src/core/selectioncontext.js' // <-- YOUR NEW FILE
+    'src'core/ui/workspace.js',
+    'src/core/selectioncontext.js',
+    'src/core/procedural/lighting.js' // <-- ADDED
     
     // Add icons/ folder here when ready
     // 'icons/icon-192x192.png',
