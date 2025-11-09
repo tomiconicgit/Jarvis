3d520fe52400843c8cafc1e10f4faf224d3c3ca4
tomiconicgit <tomiconicx@icloud.com>
2025-11-09 15:43:05 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -34,4 +34,35 @@ const FILES_TO_CACHE = [
     // 'icons/icon-maskable-512x512.png'
 ];
 
-// ... (rest of the file is unchanged) ...
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('[ServiceWorker] Caching app shell');
+                return cache.addAll(FILES_TO_CACHE);
+            })
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keyList) =>
+            Promise.all(
+                keyList.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        console.log('[ServiceWorker] Removing old cache', key);
+                        return caches.delete(key);
+                    }
+                })
+            )
+        ).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
